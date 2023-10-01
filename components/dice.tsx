79bdacd9a9516cd60b2192b268ed628b44995176
M Wang <wang.matthew.h@gmail.com>
2023-09-30 23:14:54 -0400

    localStorage for dice, load skeleton

--- components/dice.tsx
+++ components/dice.tsx
@@ -1,7 +1,7 @@
 "use client"
-import { Box, Button, Fab, IconButton } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import clsx from "clsx";
-import { useReducer, useState } from "react";
+import { useReducer, useState, useEffect } from "react";
 import styles from './dice.module.css';
 
 type DieColor = "white1"|"white2"|"red"|"yellow"|"blue"|"green";
@@ -36,11 +36,27 @@ export function rollDice(colors: DieColor[] ) : DieRoll[] {
 
 export default function Dice() {
 
-    const [dice, setDice] = useState<DieRoll[]>(ALL_DICE_COLORS.map((color, i) => {return {color, face: i + 1 as DieFace}}));
+    const [diceState, setDiceState] = useState<DieRoll[]>(ALL_DICE_COLORS.map((color, i) => {return {color, face: i + 1 as DieFace}}));
     const [rollCount, incrementRollCount] = useReducer((n : number) => n + 1, 0);
 
+    const [loadingState, endLoadingState] = useReducer(() => false, true);
+
+    
+    useEffect(() => {
+        if (loadingState) {
+            const storedState = localStorage.getItem('quixx-dice');
+            if (storedState) {
+                setDiceState(JSON.parse(storedState));
+            }
+            endLoadingState();
+            return;
+        }
+
+        localStorage.setItem('quixx-dice', JSON.stringify(diceState));
+    }, [diceState]);
+
     function reroll(){
-        setDice(rollDice(ALL_DICE_COLORS));
+        setDiceState(rollDice(ALL_DICE_COLORS));
         incrementRollCount();
     }
 
@@ -48,18 +64,36 @@ export default function Dice() {
         <Box className="bg-slate-100 border border-black border-dashed rounded-xl w-full h-full my-2">
             {/* <Button variant="contained">Roll Dice</Button> */}
             <div className="flex flex-auto justify-around space-x-1 w-full items-center">
-            <Button color="primary" variant="contained" onClick={reroll}  aria-label="roll" className="float-left text-lg">
+
+            <Button color="primary" variant="contained" onClick={reroll}  aria-label="roll" className="float-left text-lg mx-2"
+                disabled={loadingState}>
                 Roll {'\u{1F3B2}'}
             </Button>
-            {dice.map((die, i) => <Die key={die.color + rollCount} {...die}/>)}
+            {diceState.map((die, i) => 
+                loadingState ? 
+                    <DieSkeleton key={die.color + rollCount} {...die}/>
+                :
+                    <Die key={die.color + rollCount} {...die}/> 
+            )}
             </div>
         </Box>
     );
 };
 
+function DieSkeleton({color} : { color: DieColor}){
+    return (
+
+        <div role="status" className={clsx(DICE_COLORCLASS.get(color),"text-8xl select-none animate-pulse flex-auto")}> 
+            {'\u{25FB}'}
+        </div>
+
+    )
+}
+
+
 function Die({face, color} : {face: DieFace, color: DieColor}){
     return (
-        <div className={clsx(DICE_COLORCLASS.get(color),"text-8xl select-none", styles.die)}>
+        <div className={clsx(DICE_COLORCLASS.get(color),"text-8xl select-none flex-auto", styles.die)}>
             {DICE_UNICODE.get(face)}
         </div>
     )
